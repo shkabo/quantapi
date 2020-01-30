43044c48ba444d9c1725098397376b3296bba6fc
Bosko Stupar <bosko.stupar@gmail.com>
2020-01-30 21:13:14 +0100

    rabbit mq bundle & conf

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
 ];
